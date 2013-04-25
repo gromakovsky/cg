ab5c9aa8f0e3e5aae710f7922a92788e3c5edfec
Ivan Gromakovsky <gie1994@gmail.com>
2013-04-25 20:08:38 +0400

    Fixed andrew.h

--- include/cg/convex_hull/andrew.h
+++ include/cg/convex_hull/andrew.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iterator>
 #include <algorithm>
 
 #include <cg/operations/orientation.h>
@@ -13,13 +14,13 @@ namespace cg
    {
       if (p == q)
          return p;
-      if (boost::next(p) == q)
+      if (std::next(p) == q)
          return q;
 
       std::iter_swap(p, std::min_element(p, q));
-      std::iter_swap(boost::next(p), std::max_element(p, q));
-      auto bound = std::partition(boost::next(boost::next(p)), q, [p] (point_2 const & x) {
-         return orientation(x, *p, *boost::next(p)) != CG_LEFT;
+      std::iter_swap(std::next(p), std::max_element(p, q));
+      auto bound = std::partition(std::next(std::next(p)), q, [p] (point_2 const & x) {
+         return orientation(x, *p, *std::next(p)) != CG_LEFT;
       });
       std::sort(p, bound);
       std::sort(bound, q, std::greater<typename std::iterator_traits<_RandomAccessIterator>::value_type>());
