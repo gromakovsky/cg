077b98219fd1c796b0099c5a75a7939d91f17334
Ivan Gromakovsky <gie1994@gmail.com>
2013-10-12 17:59:24 +0400

    Reworked visibility_graph

--- examples/visibility_graph.cpp
+++ examples/visibility_graph.cpp
@@ -20,7 +20,7 @@ using cg::segment_2f;
 
 struct visibility_graph_viewer : cg::visualization::viewer_adapter
 {
-   visibility_graph_viewer() : moved(false)
+   visibility_graph_viewer() : polygons(1)
    {
    }
 
@@ -28,51 +28,84 @@ struct visibility_graph_viewer : cg::visualization::viewer_adapter
    {
       drawer.set_color(Qt::white);
 
-      for (auto it = points_.cbegin(); it != points_.cend(); ++it)
+      for (auto it = polygons.cbegin(); it != polygons.cend(); ++it)
       {
-         drawer.draw_point(*it);
-      }
+         if (it->size() == 0)
+         {
+            continue;
+         }
 
-      drawer.set_color(Qt::red);
+         if (it->size() == 1)
+         {
+            drawer.draw_point(it->front());
+            continue;
+         }
 
-      for (auto it = obstacles.cbegin(); it != obstacles.cend(); ++it)
-      {
-         drawer.draw_line(*it);
+         auto pol = *it;
+
+         for (size_t lp = pol.size() - 1, l = 0; l != pol.size(); lp = l++)
+         {
+            drawer.draw_point(pol[l], 3);
+            drawer.draw_line(pol[lp], pol[l]);
+         }
+
       }
 
       drawer.set_color(Qt::blue);
 
-      for (size_t i = 0; i != points_.size(); ++i)
+      for (auto it = edges.cbegin(); it != edges.cend(); ++it)
       {
-         for (size_t j = 0; j != points_.size(); ++j)
-         {
-            if (graph[i][j])
-            {
-               drawer.draw_line(segment_2f(points_[i], points_[j]));
-            }
-         }
+         drawer.draw_line(it->first, it->second);
       }
+
    }
 
    void print(cg::visualization::printer_type & p) const
    {
-      p.corner_stream() << "press mouse rbutton to add vertex" << cg::visualization::endl
-                        << "press and move mouse rbutton to add segment" << cg::visualization::endl
+      p.corner_stream() << "press N to start new polygon" << cg::visualization::endl
+                        << "press mouse rbutton to add vertex" << cg::visualization::endl
                         << "double click clears the screen" << cg::visualization::endl;
    }
 
    bool on_double_click(const point_2 & p)
    {
-      points_.clear();
-      obstacles.clear();
+      polygons.clear();
+      return true;
+   }
+
+   bool test_visibility(const point_2 & a, const point_2 & b)
+   {
+      for (auto it = polygons.cbegin(); it != polygons.cend(); ++it)
+      {
+
+         if (it->size() <= 1)
+         {
+            continue;
+         }
+
+         auto pol = *it;
+
+         for (size_t lp = pol.size() - 1, l = 0; l != pol.size(); lp = l++)
+         {
+            if (pol[lp] == a || pol[l] == a || pol[lp] == b || pol[l] == b)
+            {
+               continue;
+            }
+
+            if (cg::has_intersection(segment_2f(pol[lp], pol[l]), segment_2f(a, b)))
+            {
+               return false;
+            }
+         }
+      }
+
       return true;
    }
 
    void update()
    {
-      size_t n = points_.size();
-      size_t m = obstacles.size();
-      graph.assign(n, std::vector<bool>(n, true));
+      size_t n = polygons.size();
+      edges.clear();
 
       for (size_t i = 0; i != n; ++i)
       {
@@ -80,82 +113,79 @@ struct visibility_graph_viewer : cg::visualization::viewer_adapter
          {
             if (i == j)
             {
-               graph[i][j] = false;
                continue;
             }
 
-            for (size_t k = 0; k != m; ++k)
+            auto pol1 = polygons[i];
+            auto pol2 = polygons[j];
+
+            for (size_t a = 0; a != pol1.size(); ++a)
             {
-               if (cg::has_intersection(obstacles[k], segment_2f(points_[i], points_[j])))
+               for (size_t b = 0; b != pol2.size(); ++b)
                {
-                  graph[i][j] = false;
+
+                  if (test_visibility(pol1[a], pol2[b]))
+                  {
+                     edges.push_back(std::make_pair(pol1[a], pol2[b]));
+                  }
                }
             }
+
          }
       }
    }
 
    bool on_key(int key)
    {
-      /*      switch (key)
-            {
-            case Qt::Key_Plus :
-               eps *= 1.5;
-               break;
+      switch (key)
+      {
+      case Qt::Key_N :
+         polygons.push_back(std::vector<point_2>());
+         break;
 
-            case Qt::Key_Minus :
-               eps /= 1.5;
-               break;
+      default :
+         return false;
+      }
 
-            case Qt::Key_Q :
-               simplified ^= 1;
-               break;
-
-            default :
-               return false;
-            }
-
-            update();
-            return true;*/
-      return false;
+      return true;
    }
 
    bool on_press(const point_2f & p)
    {
-      moved = false;
-      clicked = p;
+      bool new_obs = false;
+      point_2 last;
+
+      if (polygons.back().size() != 0)
+      {
+         new_obs = true;
+         last = polygons.back().back();
+      }
+
+      polygons.back().push_back(p);
+
+      if (new_obs)
+      {
+         obstacles.push_back(segment_2f(last, p));
+      }
+
+      update();
       return true;
    }
 
    bool on_release(const point_2f & p)
    {
-      if (!moved)
-      {
-         points_.push_back(clicked);
-      }
-      else
-      {
-         obstacles.push_back(segment_2f(clicked, p));
-      }
-
-      update();
-
-      moved = false;
-      return true;
+      return false;
    }
 
    bool on_move(const point_2f & p)
    {
-      moved = true;
-      return true;
+      return false;
    }
 
 private:
-   std::vector<point_2> points_;
+   std::vector< std::vector<point_2> > polygons;
    std::vector<segment_2f> obstacles;
-   bool moved;
-   point_2 clicked;
-   std::vector< std::vector<bool> > graph;
+   std::vector< std::pair<point_2, point_2> > edges;
 };
 
 int main(int argc, char ** argv)
